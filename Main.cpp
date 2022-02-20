65e31db59e3d8cf2df6364b5b57325ec0e76bcd4
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2022-02-20 14:45:00 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,11 @@
-﻿#include "Father.h"
+#include "Father.h"
 
+// The state pattern is a behavioral software design pattern that allows an object
+// to alter its behavior when its internal state changes.
+// This pattern is close to the concept of finite-state machines.
+// The state pattern can be interpreted as a strategy pattern, which is able to switch a strategy
+// through invocations of methods defined in the pattern's interface.
+	
 int main()
 {
 	system("chcp 1251>0");
